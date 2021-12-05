543ca127321b3aa68597adff610a25d9043723f0
B <89058210+brilliantB@users.noreply.github.com>
2021-12-05 22:37:34 +0900

    Delete src directory

--- src/components/common/text/InputLable.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const InputLable = styled.div`
-  font-size: 1.6rem;
-  color: grey;
-  margin-bottom: 1rem;
-`;
-
-export default InputLable;
--- src/pages/Intern.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Intern() {
-  return <div>인턴 채용공고</div>;
-}
